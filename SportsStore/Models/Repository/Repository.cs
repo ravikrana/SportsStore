15a23f0056a4cdf8e968592060d9b83646636831
RAVI KUMAR RANA <ravikrana@live.com>
2014-08-09 21:31:21 +0530

    Connectivity With Database

---
+++ SportsStore/Models/Repository/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models.Repository
+{
+    public class Repository
+    {
+        private EFDbContext content = new EFDbContext();
+        public IEnumerable<Product> Products { get { return content.Products; } }
+    }
+}
